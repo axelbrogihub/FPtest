8627211c4e4b286f9196861fd56dd6f65c608975
axelbrogihub <axelbrogipinilla@gmail.com>
2025-01-07 21:20:22 -0300

    ejercicios varios de if y else

--- test.py
+++ test.py
@@ -1,1 +1,122 @@
-print("Hola mundo")
+# nombre="Axel BROGI"
+# edad=23
+
+# print("Mi nombre es", nombre, "y mi edad es ", edad)
+
+
+
+
+# print("ingrese un numero")
+# num1=int(input())
+
+# print("ingrese otro un numero")
+# num2=int(input())
+
+
+
+# print(num1+(5*num2+8))
+
+
+
+# print("ingrese un numero")
+# num1=int(input())
+
+# if num1>0:
+#     print("el numero ingresado es positivo")
+# else:
+#     print("el numero ingresado es negativo o cero")
+
+
+# print("ingrese su edad")
+# num1=int(input())
+
+# if num1>18:
+#     print("es mayor de edad")
+# else:
+#     print("no es mayor de edad")
+    
+    
+# num1=0
+# num2=0
+# num3=0
+
+
+# print("ingrese el primer numero")
+# num1=int(input())
+
+
+# print("ingrese el segundo numero")
+# num2=int(input())
+
+
+# print("ingrese el tercer numero numero")
+# num3=int(input())
+
+
+# print((num1+num2+num3)/3)
+
+
+
+# un parque nacional que tiene los siguientes valores
+
+# edad=0
+# entradas=0
+
+# # menor de 12 $500
+# # entre 13 y 18 $1000
+# # entre 19 y 64 $2000
+# # mas de 65 $700
+
+# print(f"bienvenido al parque nacional")
+
+# print("cual es su edad?")
+
+# edad=int(input())
+
+# if edad<12:
+#     print("su entrada cuesta 500 pesos")
+#     precio=500
+# elif edad>=13 and edad<=18:
+#     print("debe pagar 1000 pesos")
+#     precio=1000
+# elif edad>=19 and edad<=64:
+#     print("ud debera pagar 2000 pesos")
+#     precio=2000
+# elif edad>65:
+#     print("debera pagar 700 pesos")
+#     precio=700
+# else:
+#     print("no puede pasar")
+    
+ 
+# entradas=0
+   
+# print("cuantas entradas desea?")
+# print("ingrese el total de entradas")
+# cantidad=int(input())
+
+# total=cantidad*precio
+
+# print("su total a pagar es", total)
+
+
+# entradas(int(input("ingrese cuantas entradas desea"))) 
+
+
+
+print("BIENVENIDO A LA SUCURSAL")
+
+direccion=input("ud pertenece a la comuna de la florida?")
+
+if direccion == "si":
+    print("puede continuar a la siguiente")
+    
+    
+carnet=input("ud tiene carnet de socio?")
+
+if carnet =="si":
+    print=int(input("ingrese su numero de carnet"))
+    
+    
+
+
