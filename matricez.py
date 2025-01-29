3373590d57ef7a106a7058551ebc95d2e8b4dabe
axelbrogihub <axelbrogipinilla@gmail.com>
2025-01-28 23:27:38 -0300

    clase del 28 de enero con archivostxt

--- matricez.py
+++ matricez.py
@@ -122,3 +122,80 @@
 # print(f"el nombre con la menor cantidad de caracteres es: {nombre_menor_longitud}, este nombre sera eliminado")
 
 
+
+
+### clase del 28 de enero archivos txt.
+
+fut=" Algo mas de texto por aca"
+
+lista=["Mario", "Luigui", "Peach", "TOAD"]
+
+with open('mi_archivo.txt', 'a') as archivo:
+    for i in lista:
+        archivo.write(f"{i}\n")
+    
+    
+# archivo = open('mi_archivo.txt', 'r')
+# contenido = archivo.read()
+# print(contenido)
+# archivo.close()
+
+# with open('mi_archivo.txt', 'a') as archivo:
+#     for elemento in mi_lista:
+#         archivo.write(elemento + '\n')
+
+
+# import json
+# # Datos JSON
+# datos = {
+# "nombre": "Esteban",
+# "edad": 25,
+# "comuna": "Santiago",
+# "estudios": ["colegio Arturo Prat", "liceo el bosque",
+# "Duoc UC", "Diplomado Duoc UC"]
+# }
+# # Abre el archivo, w es escritura
+# with open('archivo.json', 'w') as archivo:
+#     json.dump(datos, archivo)
+    
+    
+
+
+
+    
+# personas = {}
+
+
+
+# while True:
+
+#   nomb = input("Ingrese su nombre: ")
+
+#   eda = int(input("Ingrese su edad: "))
+
+#   personas[nomb] = eda
+
+  
+
+#   salir = input("Para salir presione 'x', para continuar presione enter ")
+
+#   if salir == "x":
+
+#     break
+
+#   else:
+
+#     print("Siga ingresando nombres y edades.")
+
+    
+
+# print("Lista de personas ingresadas:")
+
+# for nombre, edad in personas.items():
+
+#   print(f"Nombre: {nombre}, Edad: {edad} años")
+  
+# print(personas)
+
+
+#boleta en archivo .txt
